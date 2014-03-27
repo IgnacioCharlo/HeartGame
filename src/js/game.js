8ff9a8bb7c7f2b9e8a92ea33e1e62fa01310f174
IgnacioCharlo <i.charlomillan@gmail.com>
2014-03-27 10:38:39 -0400

    minor changes

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,7 @@
 
     function Game() {
       this.player = null;
+      this.bars = null;
       this.bar = null;
     }
 
@@ -15,9 +16,18 @@
           , y = this.game.height / 2;
 
         this.player = this.add.sprite(x, y, 'player');
-        this.bar = this.add.sprite(0, 400, 'bar');
-        
+
+        this.bars = this.add.group();
+        this.bars.enablebody = true;
+        this.bars.physicsBodyType = Phaser.Physics.ARCADE;
+        //sprite(0, 400, 'bar');
+        this.bar = this.bars.create(0, 200, 'bar');
+        this.bar = this.bars.create(0, 400, 'bar');
         this.game.physics.enable( [this.bar, this.player], Phaser.Physics.ARCADE);
+        //Aqui he intentado poner "bars" para que se aplique al grupo entero, pero no me deja, y con "bar" solo se aplica a la ultima "bar" 
+        //que hayamos creado. A parte, no he podido trabajar mucho más porque la puta maquina virtual se me quedaba pillada todo el tiempo, y lo
+        //de github también ha sido una movida que no he podido arreglar sin ayuda de Sean
+
         this.bar.body.immovable = true;
         this.bar.body.collideWorldBounds = true;
         
@@ -35,7 +45,7 @@
         cx = this.world.centerX;
         cy = this.world.centerY;
        
-        this.game.physics.arcade.collide(this.player, this.bar);
+        this.game.physics.arcade.collide(this.player, this.bars);
 
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
           this.player.body.velocity.setTo(-200,0);
