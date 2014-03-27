b6bd38a620669fe0190a7df538da82bfc46bca4d
IgnacioCharlo <i.charlomillan@gmail.com>
2014-03-27 08:35:21 -0400

    player moves

--- src/js/game.js
+++ src/js/game.js
@@ -1,49 +1,62 @@
-(function() {
-  'use strict';
+  (function() {
+    'use strict';
 
-  function Game() {
-    this.player = null;
-    this.Barra = null;
-  }
+    function Game() {
+      this.player = null;
+      this.bar = null;
+    }
 
-  Game.prototype = {
+    Game.prototype = {
 
-    create: function () {
-    	this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    	
-      var x = this.game.width / 2
-        , y = this.game.height / 2;
+      create: function () {
+      	this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      	
+        var x = this.game.width / 2
+          , y = this.game.height / 2;
 
-      this.player = this.add.sprite(x, y, 'player');
-      this.Barra = this.add.sprite(0, 400, 'Barra');
+        this.player = this.add.sprite(x, y, 'player');
+        this.bar = this.add.sprite(0, 400, 'bar');
+        
+        this.game.physics.enable( [this.bar, this.player], Phaser.Physics.ARCADE);
+        this.bar.body.immovable = true;
+        this.bar.body.collideWorldBounds = true;
+        
+        this.player.body.collideWorldBounds = true;
+        this.player.body.bounce.set(0.0);
+        this.player.body.gravity.y = 100;
+        
+      },
+
+      update: function () {
+        var x, y, cx, cy, dx, dy, angle, scale;
+
+        x = this.input.position.x;
+        y = this.input.position.y;
+        cx = this.world.centerX;
+        cy = this.world.centerY;
+       
+        this.game.physics.arcade.collide(this.player, this.bar);
+
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+          this.player.body.velocity.setTo(-200,0);
+        }
+        else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+          this.player.body.velocity.setTo(200,0);
+        }
+        else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+          this.player.body.velocity.setTo(0,-200);
+        }
+        else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+          this.player.body.velocity.setTo(0,200);
+        }
+
+      },
+
       
-      //this.game.physics.arcade.gravity.y = 100;
-      this.game.physics.enable( [this.Barra, this.player], Phaser.Physics.ARCADE);
-      this.Barra.body.immovable = true;
-      this.Barra.body.collideWorldBounds = true;
-      
-      this.player.body.collideWorldBounds = true;
-      this.player.body.bounce.y = 0.5;
-      this.player.body.gravity.y = 100;
-      
-    },
 
-    update: function () {
-      var x, y, cx, cy, dx, dy, angle, scale;
+    };
 
-      x = this.input.position.x;
-      y = this.input.position.y;
-      cx = this.world.centerX;
-      cy = this.world.centerY;
-     
-      this.game.physics.arcade.collide(this.player, this.Barra);
-    },
+    window['plataformer'] = window['plataformer'] || {};
+    window['plataformer'].Game = Game;
 
-    
-
-  };
-
-  window['plataformer'] = window['plataformer'] || {};
-  window['plataformer'].Game = Game;
-
-}());
+  }());
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -15,7 +15,7 @@
       this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
       this.load.setPreloadSprite(this.asset);
       //this.load.image('player', 'assets/player.png');
-      this.load.image('Barra', 'assets/Barra.png');
+      this.load.image('bar', 'assets/bar.png');
       this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
     },
 
